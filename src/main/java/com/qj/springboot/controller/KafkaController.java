e73d43ce876a89d311aa508f48c92bf2efbd51da
qinjian <qinjian0306@163.com>
2018-04-22 16:44:47 +0800

    cancel

--- src/main/java/com/qj/springboot/controller/KafkaController.java
+++ src/main/java/com/qj/springboot/controller/KafkaController.java
@@ -1,39 +1,28 @@
 package com.qj.springboot.controller;
 
-import com.qj.springboot.bean.Order;
+import com.qj.springboot.kafka.KafkaInstant;
+import com.qj.springboot.kafka.model.KafkaOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.math.BigDecimal;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
+/**
+ * kafka controller
+ */
 @RestController
 public class KafkaController {
 
     @Autowired
-    private KafkaProducer kafkaProducer;
+    private KafkaInstant kafkaInstant;
 
-    @RequestMapping("/sendString")
-    public String sendSting(){
-        kafkaProducer.sendStirng("i am produce :" + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
-        return "kafka";
-    }
-
-    @RequestMapping("/sendBean")
-    public String sendBean(){
-        Order order = new Order();
-        order.setFid(Long.valueOf(1));
-        order.setFtype(1);
-        order.setFfx(new BigDecimal(0.6));
-        order.setFcoinid(20);
-        order.setFaccountcoinid(10);
-        order.setFprice(new BigDecimal(90));
-        order.setFquantity(new BigDecimal(20));
-        order.setFcreatetime(System.currentTimeMillis());
-        kafkaProducer.sendBean(order);
-        return "kafka";
+    @RequestMapping("/kafka")
+    public String sendKafkaOrder(){
+        KafkaOrder kafkaOrder = new KafkaOrder();
+        kafkaOrder.setCryptoCode("BTC");
+        kafkaOrder.setExpireTime(System.currentTimeMillis());
+        kafkaOrder.setCreateTime(System.currentTimeMillis());
+        kafkaInstant.sendOrder(kafkaOrder);
+        return "success";
     }
 
 }
